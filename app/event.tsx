e01da3013cb7eddaa93c97bd6d8c1d6ccfe1cafe
karenzhao35 <karenzhao35@gmail.com>
2024-01-31 12:03:02 -0800

    fix: fixed bug in event item

--- app/event.tsx
+++ app/event.tsx
@@ -1,10 +1,25 @@
-export default function Event(props) {
-    return (
-      <div className="flex items-center justify-center">
-        <h2>{props.name}</h2>
-        <h3>{props.start} to {props.end}</h3>
-        <h3>Seeking {props.volunteers} volunteers</h3>
-        <p> {props.description} </p>
-        </div>
-    )
-  }
+
+
+import React from 'react';
+
+interface EventProps {
+  id: string;
+  name: string;
+  start: string;
+  end: string;
+  volunteers: number;
+  description: string;
+}
+
+const Event: React.FC<EventProps> = (props) => {
+  return (
+    <div className="flex items-center justify-center">
+      <h2>{props.name}</h2>
+      <h3>{props.start} to {props.end}</h3>
+      <h3>Seeking {props.volunteers} volunteers</h3>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+export default Event;
