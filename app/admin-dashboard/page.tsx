2a5c2a17e337e886533b0bad5692e781776db049
Janaye Cheong <janaye.c13@gmail.com>
2024-02-25 01:41:33 -0800

    feat: added middleware authentication

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -3,6 +3,9 @@ import Dashboard from './dashboard'
 
 export default function Page() {
   return (
+    <div>
+    <h1>This is the Admin Dashboard</h1>
       <Dashboard></Dashboard>
+    </div>
   )
 }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { JWT } from "next-auth/jwt"
+
 
 export const authOptions = {
     providers: [
@@ -8,7 +10,14 @@ export const authOptions = {
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
     ],
+    strategy: 'jwt',
     secret: process.env.SECRET,
+    callbacks: {
+      async jwt({ token }: {token: JWT, }): Promise<JWT> {
+        token.role = "admin";
+        return token;
+  }
+    },
 }
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
