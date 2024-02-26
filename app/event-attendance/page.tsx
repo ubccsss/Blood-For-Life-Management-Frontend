a7be5632066b80bf9dd16299c44f43988105344f
Mave Hur <mave.hur@agilebits.com>
2024-02-25 21:47:44 -0500

    add use client for hydration

--- app/event-attendance/page.tsx
+++ app/event-attendance/page.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+'use client';
+import React, { useState} from 'react';
 import EventAttendance from './EventAttendance';
 
 const EventAttendancePage = () => {
