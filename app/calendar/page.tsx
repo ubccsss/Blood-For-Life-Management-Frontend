79b60b96b74620f7d4cd4074dfb8f23c6cdc15e5
karenzhao35 <karenzhao35@gmail.com>
2024-03-21 23:01:21 -0700

    feat: created skeleton for the calendar

---
+++ app/calendar/page.tsx
@@ -0,0 +1,14 @@
+
+import EventCalendar from '../../components/EventCalendar'
+import '/app/styles/event-calendar.css'
+export default function Home() {
+ 
+  return (
+
+    <div className="App">
+      <main>
+        <EventCalendar />
+      </main>
+    </div>
+  )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+
+import dayjs from 'dayjs'
 import Image from 'next/image'
 import FormDialog from './dashboard'
+import EventCalendar from '../../components/EventCalendar'
+
 export default function Home() {
+  const day = dayjs("09:00:00", "HH:mm:ss");
   return (
+    <div style={{background: "blue"}}>
     <FormDialog></FormDialog>
+    <EventCalendar dateFormat={day} />
+    </div>
+
   )
 }
