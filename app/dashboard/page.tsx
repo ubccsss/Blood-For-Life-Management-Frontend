8952160479fd35625b4d42a492652b080676c9a7
karenzhao35 <karenzhao35@gmail.com>
2024-03-29 23:29:33 -0700

    feat: added basic calendar model to display sample events

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <div style={{background: "blue"}}>
     <FormDialog></FormDialog>
-    <EventCalendar dateFormat={day} />
     </div>
 
   )
--- components/EventCalendar.tsx
+++ components/EventCalendar.tsx
@@ -4,11 +4,41 @@ import React from "react";
 import * as dateFns from "date-fns";
 
 class EventCalendar extends React.Component {
+  // sample events:
+
+  events = [
+    {
+      id: 1,
+      name: 'Fundraiswer',
+      startDate: new Date(),
+      endDate: new Date(),
+      volunteers: 5,
+      description: 'Krsipy Kreme fundraiser at the nest outside of room 4021.\n'
+    },
+  ];
+
   state = {
+    currentYear: new Date(),
     currentMonth: new Date(),
     selectedDate: new Date()
   };
 
+  // in progress while i get more details
+  renderToggle() {
+
+    return (
+      <div className="toggle row flex-middle">
+        <div className="col col-center" onClick={this.changeMonth}>
+
+        </div>
+
+        <div className="col col-center" onClick={this.changeYear}>
+
+        </div>
+
+      </div>
+    )
+  }
   renderHeader() {
     const dateFormat = "MMMM yyyy";
 
@@ -16,42 +46,61 @@ class EventCalendar extends React.Component {
       <div className="header row flex-middle">
         <div className="col col-start">
           <div className="icon" onClick={this.prevMonth}>
-            chevron_left
+            prev
           </div>
         </div>
         <div className="col col-center">
           <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
         </div>
         <div className="col col-end" onClick={this.nextMonth}>
-          <div className="icon">chevron_right</div>
+          <div className="icon">next</div>
         </div>
       </div>
     );
   }
 
   renderDays() {
-    const dateFormat = "dddd";
     const days = [];
 
-    let startDate = dateFns.startOfWeek(this.state.currentMonth);
-
-    for (let i = 0; i < 7; i++) {
-      days.push(
-        <div className="col col-center" key={i}>
-          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+    days.push(
+    <div className="col col-center" key={0}>
+       {"sunday"} 
+    </div>);
+    days.push(
+        <div className="col col-center" key={1}>
+           {"monday"} 
         </div>
-      );
-    }
+        );
+    days.push(
+        <div className="col col-center" key={2}>
+           {"tuesday"} 
+        </div>);
+    days.push(
+        <div className="col col-center" key={3}>
+           {"wednesday"} 
+        </div>);
+    days.push(
+        <div className="col col-center" key={4}>
+           {"thrusday"} 
+        </div>);
+    days.push(
+        <div className="col col-center" key={5}>
+           {"friday"} 
+        </div>);
+    days.push(
+        <div className="col col-center" key={6}>
+           {"saturday"} 
+        </div>);
 
     return <div className="days row">{days}</div>;
   }
 
   renderCells() {
     const { currentMonth, selectedDate } = this.state;
-    const monthStart = dateFns.startOfMonth(currentMonth);
-    const monthEnd = dateFns.endOfMonth(monthStart);
-    const startDate = dateFns.startOfWeek(monthStart);
-    const endDate = dateFns.endOfWeek(monthEnd);
+    const monthStart = dateFns.startOfMonth(currentMonth);// first date in curr month
+    const monthEnd = dateFns.endOfMonth(monthStart);      // last date in curr month
+    const startDate = dateFns.startOfWeek(monthStart);    // first date
+    const endDate = dateFns.endOfWeek(monthEnd);          // last date
 
     const dateFormat = "d";
     const rows = [];
@@ -71,17 +120,17 @@ class EventCalendar extends React.Component {
                 ? "disabled"
                 : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
             }`}
-            ke-y={day}
             onClick={() => this.onDateClick(cloneDay)}
           >
             <span className="number">{formattedDate}</span>
             <span className="bg">{formattedDate}</span>
+            <span className="event">{events}</span>
           </div>
         );
         day = dateFns.addDays(day, 1);
       }
       rows.push(
-        <div className="row" key={day}>
+        <div className="row">
           {days}
         </div>
       );
@@ -90,7 +139,7 @@ class EventCalendar extends React.Component {
     return <div className="body">{rows}</div>;
   }
 
-  onDateClick = day => {
+  onDateClick = (day: Date) => {
     this.setState({
       selectedDate: day
     });
